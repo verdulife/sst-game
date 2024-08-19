896ffce8fa052a4229ca5a4901308498c46ce956
verdulife <verdu@live.com>
2024-08-19 16:31:46 +0200

    add fllor and movement 8d

--- src/main.js
+++ src/main.js
@@ -2,10 +2,13 @@ import 'phaser';
 
 const config = {
     type: Phaser.AUTO,
-    width: 1024,
-    height: 576,
+    width: 1920,
+    height: 1080,
     parent: 'game-container',
     backgroundColor: '#000000',
+    physics: {
+        default: 'arcade',
+    },
     scale: {
         mode: Phaser.Scale.FIT,
         autoCenter: Phaser.Scale.CENTER_BOTH
@@ -19,6 +22,10 @@ const config = {
 
 new Phaser.Game(config);
 
+function getMagnitude(x, y) {
+    return Math.sqrt(Math.pow(x, 2) + Math.pow(y, 2));
+}
+
 function preload() {
     this.load.image('floor', 'assets/floor.jpg');
     this.load.spritesheet('player-1', 'assets/player.png', {
@@ -28,22 +35,33 @@ function preload() {
 }
 
 function create() {
-    this.add.image(400, 300, 'floor');
-    this.player = this.add.sprite(400, 300, 'player-1');
+    this.add.tileSprite(0, 0, config.width, config.height, 'floor').setOrigin(0, 0);
+
+    this.player = this.physics.add.sprite(400, 300, 'player-1').setCollideWorldBounds(true);
 
     this.anims.create({
-        key: 'player-idle',
-        frames: [{
-            key: 'player-1',
-            frame: 20
-        }],
+        key: 'player-up',
+        frames: this.anims.generateFrameNumbers('player-1', {
+            start: 0,
+            end: 7
+        }),
+        repeat: -1
+    });
+
+    this.anims.create({
+        key: 'player-down',
+        frames: this.anims.generateFrameNumbers('player-1', {
+            start: 32,
+            end: 39
+        }),
+        repeat: -1
     });
 
     this.anims.create({
         key: 'player-left',
         frames: this.anims.generateFrameNumbers('player-1', {
-            start: 0,
-            end: 3
+            start: 48,
+            end: 55
         }),
         repeat: -1
     });
@@ -51,8 +69,44 @@ function create() {
     this.anims.create({
         key: 'player-right',
         frames: this.anims.generateFrameNumbers('player-1', {
-            start: 4,
-            end: 7
+            start: 16,
+            end: 23
+        }),
+        repeat: -1
+    });
+
+    this.anims.create({
+        key: 'player-down-left',
+        frames: this.anims.generateFrameNumbers('player-1', {
+            start: 40,
+            end: 47
+        }),
+        repeat: -1
+    });
+
+    this.anims.create({
+        key: 'player-down-right',
+        frames: this.anims.generateFrameNumbers('player-1', {
+            start: 24,
+            end: 31
+        }),
+        repeat: -1
+    });
+
+    this.anims.create({
+        key: 'player-up-left',
+        frames: this.anims.generateFrameNumbers('player-1', {
+            start: 56,
+            end: 63
+        }),
+        repeat: -1
+    });
+
+    this.anims.create({
+        key: 'player-up-right',
+        frames: this.anims.generateFrameNumbers('player-1', {
+            start: 8,
+            end: 15
         }),
         repeat: -1
     });
@@ -61,14 +115,46 @@ function create() {
 }
 
 function update() {
+    let speed = 1;
+    let diagonalSpeed = speed / getMagnitude(speed, speed);
 
-    if (this.keys.left.isDown) {
-        this.player.anims.play('player-left');
-        this.player.x -= 5;
-    } else if (this.keys.right.isDown) {
-        this.player.anims.play('player-right');
-        this.player.x += 5;
-    } else {
-        this.player.anims.play('player-idle');
+    if (this.keys.up.isDown && this.keys.left.isDown) {
+        this.player.anims.play('player-up-left', true);
+        this.player.x -= diagonalSpeed;
+        this.player.y -= diagonalSpeed;
+    }
+    else if (this.keys.up.isDown && this.keys.right.isDown) {
+        this.player.anims.play('player-up-right', true);
+        this.player.x += diagonalSpeed;
+        this.player.y -= diagonalSpeed;
+    }
+    else if (this.keys.down.isDown && this.keys.left.isDown) {
+        this.player.anims.play('player-down-left', true);
+        this.player.x -= diagonalSpeed;
+        this.player.y += diagonalSpeed;
+    }
+    else if (this.keys.down.isDown && this.keys.right.isDown) {
+        this.player.anims.play('player-down-right', true);
+        this.player.x += diagonalSpeed;
+        this.player.y += diagonalSpeed;
+    }
+    else if (this.keys.up.isDown) {
+        this.player.anims.play('player-up', true);
+        this.player.y -= speed;
+    }
+    else if (this.keys.left.isDown) {
+        this.player.anims.play('player-left', true);
+        this.player.x -= speed;
+    }
+    else if (this.keys.right.isDown) {
+        this.player.anims.play('player-right', true);
+        this.player.x += speed;
+    }
+    else if (this.keys.down.isDown) {
+        this.player.anims.play('player-down', true);
+        this.player.y += speed;
+    }
+    else {
+        this.player.anims.pause();
     }
 }
